7e22e798cfc84901871179fb4a6c4d8ebf600ef5
zhenglin_glb <zhenglin0518@gmail.com>
2019-09-06 13:53:01 +1000

    acceptance test 3

--- features/homepageFeature.feature
+++ features/homepageFeature.feature
@@ -2,4 +2,16 @@ Feature: Homepage Features
 
   Scenario: Test login button enabled
     Given I visit Used Electronics Online website
-    Then I see the login button
+    When I clik the login button
+    Then I should see the login page
+
+  Scenario Outline: Test category buttons
+    Given I visit Used Electronics Online website
+    Then I should see "<category>" button
+
+    Examples:
+      |category|
+      |laptop |
+      |phone  |
+      |camera |
+      |others |
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
 const webdriver = require('selenium-webdriver');
+var {setDefaultTimeout} = require('cucumber');
+setDefaultTimeout(80 * 1000);
 
 var driver = new webdriver.Builder().forBrowser('chrome').build();
 
@@ -8,8 +10,20 @@ Given('I visit Used Electronics Online website', function () {
 	return driver.get('https://used-electronics-online.herokuapp.com/');         
 });
 
-Then('I see the login button', function () {
+When('I clik the login button', function () {
 	driver.sleep(1000).then(function(){
-		driver.findElement(webdriver.By.name('login'));
+		driver.findElement(webdriver.By.name('login')).click();
+	});
+});
+
+Then('I should see the login page', function () {
+	driver.sleep(1000).then(function(){
+		driver.getCurrentUrl().then(url => {console.log(url);});
+	});
+});
+
+Then('I should see {string} button', function (category) {
+	driver.sleep(1000).then(function(){
+		driver.findElement(webdriver.By.name(category));
 	});
 });
