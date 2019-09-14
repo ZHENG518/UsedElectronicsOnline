9dd1b4e0eac803fe920786f04d708fdf36c7afe4
zhenglin_glb <zhenglin0518@gmail.com>
2019-09-14 12:39:04 +1000

    edit

--- test/register.test.js
+++ test/register.test.js
@@ -4,15 +4,14 @@ const registerWithEmailAndPassword = require('../js/register');
 describe('test register.js',function(){
 	describe('test registerWithEmailAndPassword fucntion', function(){
 		this.timeout(10000);
-		it('successful regist', function(){
+		
+		it('regist with email and password', function(){
 			return registerWithEmailAndPassword("unittest@gmail.com","123456").then(function(value){
 				assert.equal(value.user.email,"unittest@gmail.com");
-			});
-		  });
-		it('regist with an exist email', function(){
-			return registerWithEmailAndPassword("test@gmail.com","123456").catch(function(error){
+			}).catch(function(error){
 				assert.equal(error.message,"The email address is already in use by another account.");
 			});
-		});
+  		});
+
 	});
 });
